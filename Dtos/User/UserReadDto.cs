b056ae17f29fdef3bdf74c1417903f13c0b43344
Dominykas Šeputis <dom.seputis@gmail.com>
2021-05-24 09:01:44 +0300

    Removed ID from read DTO

--- Dtos/User/UserReadDto.cs
+++ Dtos/User/UserReadDto.cs
@@ -3,7 +3,6 @@ namespace meistrelis.Dtos.User
     public class UserReadDto
     {
         public string Fullname { get; set; }
-        public int Id { get; set; }
         public string Email { get; set; }
         public bool IsMechanic { get; set; }
         public string Phone { get; set; }
