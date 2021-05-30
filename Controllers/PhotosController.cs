ac75068c92d64bfd675b2a66ea3c9c8700cd5c12
Dominykas Šeputis <dom.seputis@gmail.com>
2021-05-30 11:28:36 +0300

    Removed logging

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -23,7 +23,6 @@ namespace meistrelis.Controllers
             req.AddJsonBody(requestData);
             req.AddHeader("Authorization", Environment.GetEnvironmentVariable("GCP") ?? "");
             var response = client.Execute(req);
-            Console.Write(response);
             var parsedContent = JsonConvert.DeserializeObject<PicturePayloadDto>(response.Content);
             var picResp =  new PictureResponseDto();
             picResp.Class = parsedContent.Payload.First().DisplayName;
